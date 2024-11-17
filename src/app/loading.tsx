54530de8395dbc5798fb399aeed22c037cbbeb27
Kasfi <kasfiheqn@gmail.com>
2024-11-18 02:45:58 +0700

    add favicon and loading components

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -5,8 +5,12 @@ import React from "react";
 
 export default function Loading() {
   return (
-    <div className="fixed inset-0 flex items-center justify-center bg-tealGreen z-50">
-      <div className="animate-spin rounded-full h-16 w-16 border-t-4 border-white"></div>
+    <div className="fixed inset-0 flex items-center justify-center z-50 overflow-hidden">
+      <div className="animate-spin rounded-full h-24 w-24 border-8 border-white overflow-hidden">
+        <div className="h-1/3 bg-primaryDarkBlue"></div>
+        <div className="h-1/3 bg-white flex items-center justify-center"></div>
+        <div className="h-1/3 bg-primaryOrange"></div>
+      </div>
     </div>
   );
 }
